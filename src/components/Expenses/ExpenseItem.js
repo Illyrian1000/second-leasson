d5b02d36dc7dd5ddc768ca1f8ee4ea82a04e935a
Illyrian1000 <52840976+Illyrian1000@users.noreply.github.com>
2023-08-25 15:20:11 +0200

    new commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  //useState
+  const [title, setTitle] = useState(props.title);
+
+  // let title = props.title;
+
+  const buttonClicked = () => {
+    setTitle("Updated");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={buttonClicked}>Change title</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,129 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  //TITLE
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
+
+  const tittleChangeHandel = (event) => {
+    setEnteredTitle(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+
+    // setUserInput((prevState) => {
+    //   return {
+    //     ...prevState,
+    //     enteredTitle: event.targed.value,
+    //   };
+    // });
+
+    console.log(event.target.value);
+  };
+
+  //AMOUNT
+  const amountChangeHandel = (event) => {
+    setEnteredAmount(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+    console.log(event.target.value);
+  };
+
+  //DATE
+  const dateChangeHandel = (event) => {
+    setEnteredDate(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+    console.log(event.target.value);
+  };
+
+  // const inputChangeHandler = (identifier, value) => {
+  //   if (identifier === "tittle") {
+  //     setEnteredTitle(value);
+  //   } else if (identifier === "date") {
+  //     setEnteredDate(value);
+  //   } else {
+  //     setEnteredAmount(value);
+  //   }
+  // };
+
+  // onChange={(event) =>
+  //   inputChangeHandler("tittle", event.target.value)
+  // }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const expenseData = {
+      tittle: enteredTitle,
+      amount: enteredDate,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+
+    setEnteredDate("");
+    setEnteredTitle("");
+    setEnteredAmount("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={tittleChangeHandel}
+          />
+        </div>
+
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onInput={amountChangeHandel}
+          />
+        </div>
+
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2024-01-01"
+            value={enteredDate}
+            onChange={dateChangeHandel}
+          />
+        </div>
+      </div>
+
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
